cdd8149f7e0a552db65feb57e4925d48dcf89e13
noisegaze <ben.stephanus@gmail.com>
2022-01-03 19:24:26 -0500

    Updated project.

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -16,7 +16,7 @@ var speakWord = "Hello";
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 
-helloSpeaker = function speak(name) {
+helloSpeaker.speak = function speak(name) {
   console.log(speakWord + " " + name);
 }
 window.helloSpeaker = helloSpeaker;
